0ef0b28da1c3d4e36ff37e0c4252bd09e89c3676
Victor Avila <82524221+VictorBriAvi@users.noreply.github.com>
2023-02-10 05:23:08 -0300

    Update LoginRouter.jsx

--- src/routes/LoginRouter.jsx
+++ src/routes/LoginRouter.jsx
@@ -19,7 +19,7 @@ const LoginRouter = () => {
       <BrowserRouter>
         <Routes>
           <Route
-            path="/PortafolioWeb"
+            path="http://VictorBriAvi.github.io/PortafolioWeb"
             element={
               <PublicRouter>
                 <LoginScreen />
